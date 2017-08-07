1ab9fae6dca734b5221967e2b7e0d29438281c92
Andrew Fitzsimons <andrewsfitzsimons@gmail.com>
2017-08-07 10:46:36 +0200

    Shows ingredients in recipe details view

--- app/src/main/java/com/afitzwa/andrew/tastybakes/RecipeDetailActivity.java
+++ app/src/main/java/com/afitzwa/andrew/tastybakes/RecipeDetailActivity.java
@@ -5,17 +5,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.afitzwa.andrew.tastybakes.data.RecipeContent;
 
 import java.util.List;
 
+import static android.support.v7.recyclerview.R.attr.layoutManager;
 import static com.afitzwa.andrew.tastybakes.data.RecipeContent.RECIPE_MAP;
 
 /**
@@ -26,6 +30,9 @@ import static com.afitzwa.andrew.tastybakes.data.RecipeContent.RECIPE_MAP;
  */
 public class RecipeDetailActivity extends AppCompatActivity {
     private static final String STEP_FRAGMENT_TAG = "STEPTAG";
+
+    private static final int VIEW_TYPE_INGREDIENTS = 0;
+    private static final int VIEW_TYPE_STEP = 1;
 
     private static final String TAG = RecipeDetailActivity.class.getSimpleName();
 
@@ -65,6 +72,9 @@ public class RecipeDetailActivity extends AppCompatActivity {
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView, RecipeContent.Recipe recipe) {
+        DividerItemDecoration dividerItemDecoration =
+                new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
         recyclerView.setAdapter(new DetailRecyclerViewAdapter(recipe.getSteps()));
     }
 
@@ -78,52 +88,96 @@ public class RecipeDetailActivity extends AppCompatActivity {
         }
 
         @Override
+        public int getItemViewType(int position) {
+            if (position == 0)
+                return VIEW_TYPE_INGREDIENTS;
+            else
+                return VIEW_TYPE_STEP;
+        }
+
+        @Override
         public RecipeStepViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.recipe_step_list_content, parent, false);
-
+            View view = null;
+            switch (viewType) {
+                case VIEW_TYPE_INGREDIENTS:
+                    view = LayoutInflater.from(parent.getContext())
+                            .inflate(R.layout.ingredient_card, parent, false);
+                    break;
+                case VIEW_TYPE_STEP:
+                    view = LayoutInflater.from(parent.getContext())
+                            .inflate(R.layout.recipe_step_list_content, parent, false);
+                    break;
+                default:
+                    // ERROR
+                    break;
+            }
             return new RecipeStepViewHolder(view);
         }
 
         @Override
         public void onBindViewHolder(final RecipeStepViewHolder holder, int position) {
 
-            holder.mStep = RECIPE_STEP.get(position);
-            holder.mStepTextView.setText(holder.mStep.getShortDesc());
 
-            holder.mView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
+            if (getItemViewType(position) == VIEW_TYPE_INGREDIENTS) {
 
-                    if (mTwoPane) {
-                        Bundle args = new Bundle();
-                        args.putString(RecipeStepFragment.ARG_RECIPE_ID, holder.mStep.getRecipe());
-                        args.putInt(RecipeStepFragment.ARG_RECIPE_STEP_ID, holder.getAdapterPosition());
+                holder.mStep = RECIPE_STEP.get(position);
 
-                        RecipeStepFragment recipeStepFragment = new RecipeStepFragment();
-                        recipeStepFragment.setArguments(args);
+                List<RecipeContent.Recipe.Ingredient> ingredients =
+                        RECIPE_MAP.get(holder.mStep.getRecipe()).getIngredients();
 
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.recipe_step_container, recipeStepFragment, STEP_FRAGMENT_TAG)
-                                .commit();
-                    } else {
+                for (RecipeContent.Recipe.Ingredient ingredient : ingredients) {
 
-                        Context context = view.getContext();
-                        Intent intent = new Intent(context, RecipeStepActivity.class);
-                        intent.putExtra(RecipeStepActivity.ARG_RECIPE_ID, holder.mStep.getRecipe());
-                        intent.putExtra(RecipeStepActivity.ARG_RECIPE_STEP_ID, holder.getAdapterPosition());
+                    RelativeLayout ingredientLayout =
+                            (RelativeLayout) View.inflate(getApplicationContext(), R.layout.ingredient, null);
 
-                        context.startActivity(intent);
-                    }
+                    TextView ingredientAmount = ingredientLayout.findViewById(R.id.ingredient_amount);
+                    TextView ingredientName = ingredientLayout.findViewById(R.id.ingredient_name);
+
+                    ingredientAmount.setText(ingredient.getmQuantity() + " " + ingredient.getmMeasure());
+                    ingredientName.setText(ingredient.getmName());
+
+                    ((LinearLayout)holder.mView).addView(ingredientLayout);
                 }
-            });
+            } else {
+                holder.mStep = RECIPE_STEP.get(position - 1);
+
+                assert holder.mStep != null;
+
+                holder.mStepTextView.setText(holder.mStep.getShortDesc());
+
+                holder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        if (mTwoPane) {
+                            Bundle args = new Bundle();
+                            args.putString(RecipeStepFragment.ARG_RECIPE_ID, holder.mStep.getRecipe());
+                            args.putInt(RecipeStepFragment.ARG_RECIPE_STEP_ID, holder.getAdapterPosition());
+
+                            RecipeStepFragment recipeStepFragment = new RecipeStepFragment();
+                            recipeStepFragment.setArguments(args);
+
+                            getSupportFragmentManager().beginTransaction()
+                                    .replace(R.id.recipe_step_container, recipeStepFragment, STEP_FRAGMENT_TAG)
+                                    .commit();
+                        } else {
+
+                            Context context = view.getContext();
+                            Intent intent = new Intent(context, RecipeStepActivity.class);
+                            intent.putExtra(RecipeStepActivity.ARG_RECIPE_ID, holder.mStep.getRecipe());
+                            intent.putExtra(RecipeStepActivity.ARG_RECIPE_STEP_ID, holder.getAdapterPosition());
+
+                            context.startActivity(intent);
+                        }
+                    }
+                });
+            }
         }
 
         @Override
         public int getItemCount() {
-            return RECIPE_STEP.size();
-        }
+            return RECIPE_STEP.size() + 1;
+        } // +1 for ingredient list
 
         public class RecipeStepViewHolder extends RecyclerView.ViewHolder {
             public final View mView;
