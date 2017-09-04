3b56470db9cef88a417d49007ac89c9f1687e41d
Andrew Fitzsimons <andrewsfitzsimons@gmail.com>
2017-09-04 13:10:08 +0200

    Loads ingredient list using CursorAdapter

--- app/src/main/java/com/afitzwa/andrew/tastybakes/RecipeDetailActivity.java
+++ app/src/main/java/com/afitzwa/andrew/tastybakes/RecipeDetailActivity.java
@@ -16,9 +16,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.afitzwa.andrew.tastybakes.data.IngredientColumns;
 import com.afitzwa.andrew.tastybakes.data.IngredientProvider;
@@ -29,6 +27,9 @@ import com.afitzwa.andrew.tastybakes.data.StepColumns;
 import com.afitzwa.andrew.tastybakes.data.StepProvider;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import static com.afitzwa.andrew.tastybakes.data.RecipeContent.RECIPE_MAP;
 
@@ -41,17 +42,24 @@ import static com.afitzwa.andrew.tastybakes.data.RecipeContent.RECIPE_MAP;
 public class RecipeDetailActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor> {
 
-    private static final String STEP_FRAGMENT_TAG = "STEP_TAG";
-    private static final int RECIPE_CURSOR_LOADER_ID = 0;
-    private static final int INGREDIENT_CURSOR_LOADER_ID = 1;
-    private static final int STEP_CURSOR_LOADER_ID = 2;
-
     private static final String TAG = RecipeDetailActivity.class.getSimpleName();
+
+    private static final String STEP_FRAGMENT_TAG = "STEP_TAG";
+
+    private static final int CURSOR_LOADER_RECIPE_ID = 0;
+    private static final int CURSOR_LOADER_INGREDIENT_ID = 1;
+    private static final int CURSOR_LOADER_STEP_ID = 2;
 
     public static final String ARG_ITEM_ID = "item_id";
 
-    public static final String ARG_RECIPE_NAME_ID = "item_id";
-    public static final String ARG_RECIPE_KEY_ID = "recipe_key_id";
+    public static final String ARG_RECIPE_NAME_ID = "recipe_name_id";
+    public static final String ARG_RECIPE_ROW_ID = "recipe_row_id";
+
+    @BindView(R.id.ingredient_list_view) ListView mIngredientListView;
+    @BindView(R.id.recipe_detail) RecyclerView mStepListView;
+    @BindView(R.id.detail_toolbar) Toolbar mToolbar;
+
+    private IngredientCursorAdapter mIngredientCursorAdapter;
 
     /**
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
@@ -66,23 +74,27 @@ public class RecipeDetailActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
 
-        Bundle b = new Bundle();
-        b.putString(
-                ARG_RECIPE_NAME_ID,
-                getIntent().getStringExtra(RecipeDetailActivity.ARG_ITEM_ID));
+        ButterKnife.bind(this);
 
-        getLoaderManager().initLoader(RECIPE_CURSOR_LOADER_ID, b, this);
+        String recipeName = getIntent().getStringExtra(RecipeDetailActivity.ARG_ITEM_ID);
+
+        Bundle b = new Bundle();
+        b.putString(ARG_RECIPE_NAME_ID, recipeName);
+
+        getLoaderManager().initLoader(CURSOR_LOADER_RECIPE_ID, b, this);
 
         mRecipe = RECIPE_MAP.get(getIntent().getStringExtra(RecipeDetailActivity.ARG_ITEM_ID));
 
-        Toolbar toolbar = findViewById(R.id.detail_toolbar);
-        toolbar.setTitle(mRecipe.getTitle());
-        setSupportActionBar(toolbar);
+        mToolbar.setTitle(mRecipe.getTitle());
+        setSupportActionBar(mToolbar);
 
-        RecyclerView recipeDetailRecyclerView = findViewById(R.id.recipe_detail);
-        assert recipeDetailRecyclerView != null;
+        mIngredientCursorAdapter = new IngredientCursorAdapter(this, null, 0);
+        mIngredientListView.setDividerHeight(0);
+        mIngredientListView.setItemsCanFocus(false);
+        mIngredientListView.setAdapter(mIngredientCursorAdapter);
+
         assert mRecipe != null;
-        setupRecyclerView(recipeDetailRecyclerView, mRecipe);
+        setupRecyclerView(mStepListView, mRecipe);
 
         if (findViewById(R.id.recipe_step_container) != null) {
             // The detail container view will be present only in the
@@ -97,26 +109,27 @@ public class RecipeDetailActivity extends AppCompatActivity implements
         DividerItemDecoration dividerItemDecoration =
                 new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
         recyclerView.addItemDecoration(dividerItemDecoration);
+
         recyclerView.setAdapter(new DetailRecyclerViewAdapter(recipe.getSteps()));
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        if (i == RECIPE_CURSOR_LOADER_ID) {
+        if (i == CURSOR_LOADER_RECIPE_ID) {
             return new CursorLoader(this,
                     RecipeProvider.Recipes.withName(bundle.getString(ARG_RECIPE_NAME_ID)),
                     null, null, null, null);
-        } else if (i == INGREDIENT_CURSOR_LOADER_ID) {
+        } else if (i == CURSOR_LOADER_INGREDIENT_ID) {
             return new CursorLoader(this,
                     IngredientProvider.Ingredients.CONTENT_URI,
                     null,
-                    IngredientColumns.RECIPE_FK + " = " + bundle.getInt(ARG_RECIPE_KEY_ID),
+                    IngredientColumns.RECIPE_FK + " = " + bundle.getInt(ARG_RECIPE_ROW_ID),
                     null, null);
-        } else if (i == STEP_CURSOR_LOADER_ID) {
+        } else if (i == CURSOR_LOADER_STEP_ID) {
             return new CursorLoader(this,
                     StepProvider.Steps.CONTENT_URI,
                     null,
-                    StepColumns.RECIPE_FK + " = " + bundle.getInt(ARG_RECIPE_KEY_ID),
+                    StepColumns.RECIPE_FK + " = " + bundle.getInt(ARG_RECIPE_ROW_ID),
                     null, null);
         } else {
             return null;
@@ -127,18 +140,19 @@ public class RecipeDetailActivity extends AppCompatActivity implements
     public void onLoadFinished(Loader<Cursor> loader, Cursor c) {
         int loaderId = loader.getId();
         switch (loaderId) {
-            case RECIPE_CURSOR_LOADER_ID:
+            case CURSOR_LOADER_RECIPE_ID:
                 Log.v(TAG, "[onLoadFinished] Recipe cursor loaded");
                 c.moveToFirst();
                 Bundle b = new Bundle();
-                b.putInt(ARG_RECIPE_KEY_ID, c.getInt(c.getColumnIndexOrThrow(RecipeColumns._ID)));
-                getLoaderManager().initLoader(INGREDIENT_CURSOR_LOADER_ID, b, this);
-                getLoaderManager().initLoader(STEP_CURSOR_LOADER_ID, b, this);
+                b.putInt(ARG_RECIPE_ROW_ID, c.getInt(c.getColumnIndexOrThrow(RecipeColumns._ID)));
+                getLoaderManager().initLoader(CURSOR_LOADER_INGREDIENT_ID, b, this);
+                getLoaderManager().initLoader(CURSOR_LOADER_STEP_ID, b, this);
                 break;
-            case INGREDIENT_CURSOR_LOADER_ID:
+            case CURSOR_LOADER_INGREDIENT_ID:
                 Log.v(TAG, "[onLoadFinished] Ingredient cursor loaded");
+                mIngredientCursorAdapter.swapCursor(c);
                 break;
-            case STEP_CURSOR_LOADER_ID:
+            case CURSOR_LOADER_STEP_ID:
                 Log.v(TAG, "[onLoadFinished] Step cursor loaded");
                 break;
             default:
@@ -154,9 +168,6 @@ public class RecipeDetailActivity extends AppCompatActivity implements
     public class DetailRecyclerViewAdapter
             extends RecyclerView.Adapter<RecipeStepViewHolder> {
 
-        private static final int VIEW_TYPE_INGREDIENTS = 0;
-        private static final int VIEW_TYPE_STEP = 1;
-
         private final List<RecipeContent.Recipe.RecipeStep> RECIPE_STEP;
 
         public DetailRecyclerViewAdapter(List<RecipeContent.Recipe.RecipeStep> recipeStep) {
@@ -164,93 +175,50 @@ public class RecipeDetailActivity extends AppCompatActivity implements
         }
 
         @Override
-        public int getItemViewType(int position) {
-            if (position == 0)
-                return VIEW_TYPE_INGREDIENTS;
-            else
-                return VIEW_TYPE_STEP;
-        }
-
-        @Override
         public RecipeStepViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = null;
-            switch (viewType) {
-                case VIEW_TYPE_INGREDIENTS:
-                    view = LayoutInflater.from(parent.getContext())
-                            .inflate(R.layout.ingredient_card, parent, false);
-                    break;
-                case VIEW_TYPE_STEP:
-                    view = LayoutInflater.from(parent.getContext())
-                            .inflate(R.layout.recipe_step_list_content, parent, false);
-                    break;
-                default:
-                    // ERROR
-                    break;
-            }
-            return new RecipeStepViewHolder(view);
+            return new RecipeStepViewHolder(LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.recipe_step_list_content, parent, false));
         }
 
         @Override
         public void onBindViewHolder(final RecipeStepViewHolder holder, int position) {
-            if (getItemViewType(position) == VIEW_TYPE_INGREDIENTS) {
+            holder.mStep = RECIPE_STEP.get(position);
 
-                holder.mStep = RECIPE_STEP.get(position);
+            assert holder.mStep != null;
 
-                List<RecipeContent.Recipe.Ingredient> ingredients =
-                        RECIPE_MAP.get(holder.mStep.getRecipeTitle()).getIngredients();
+            holder.mStepTextView.setText(holder.mStep.getShortDesc());
 
-                for (RecipeContent.Recipe.Ingredient ingredient : ingredients) {
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
 
-                    RelativeLayout ingredientLayout =
-                            (RelativeLayout) View.inflate(getApplicationContext(), R.layout.ingredient, null);
+                    if (mTwoPane) {
+                        Bundle args = new Bundle();
+                        args.putString(RecipeStepFragment.ARG_RECIPE_ID, holder.mStep.getRecipeTitle());
+                        args.putInt(RecipeStepFragment.ARG_RECIPE_STEP_ID, holder.getAdapterPosition());
 
-                    TextView ingredientAmount = ingredientLayout.findViewById(R.id.ingredient_amount);
-                    TextView ingredientName = ingredientLayout.findViewById(R.id.ingredient_name);
+                        RecipeStepFragment recipeStepFragment = new RecipeStepFragment();
+                        recipeStepFragment.setArguments(args);
 
-                    ingredientAmount.setText(ingredient.getQuantity() + " " + ingredient.getMeasure());
-                    ingredientName.setText(ingredient.getName());
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.recipe_step_container, recipeStepFragment, STEP_FRAGMENT_TAG)
+                                .commit();
+                    } else {
 
-                    ((LinearLayout) holder.mView).addView(ingredientLayout);
-                }
-            } else {
-                holder.mStep = RECIPE_STEP.get(position - 1);
+                        Context context = view.getContext();
+                        Intent intent = new Intent(context, RecipeStepActivity.class);
+                        intent.putExtra(RecipeStepActivity.ARG_RECIPE_ID, holder.mStep.getRecipeTitle());
+                        intent.putExtra(RecipeStepActivity.ARG_RECIPE_STEP_ID, holder.getAdapterPosition());
 
-                assert holder.mStep != null;
-
-                holder.mStepTextView.setText(holder.mStep.getShortDesc());
-
-                holder.mView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-                        if (mTwoPane) {
-                            Bundle args = new Bundle();
-                            args.putString(RecipeStepFragment.ARG_RECIPE_ID, holder.mStep.getRecipeTitle());
-                            args.putInt(RecipeStepFragment.ARG_RECIPE_STEP_ID, holder.getAdapterPosition() - 1);
-
-                            RecipeStepFragment recipeStepFragment = new RecipeStepFragment();
-                            recipeStepFragment.setArguments(args);
-
-                            getSupportFragmentManager().beginTransaction()
-                                    .replace(R.id.recipe_step_container, recipeStepFragment, STEP_FRAGMENT_TAG)
-                                    .commit();
-                        } else {
-
-                            Context context = view.getContext();
-                            Intent intent = new Intent(context, RecipeStepActivity.class);
-                            intent.putExtra(RecipeStepActivity.ARG_RECIPE_ID, holder.mStep.getRecipeTitle());
-                            intent.putExtra(RecipeStepActivity.ARG_RECIPE_STEP_ID, holder.getAdapterPosition() - 1);
-
-                            context.startActivity(intent);
-                        }
+                        context.startActivity(intent);
                     }
-                });
-            }
+                }
+            });
         }
 
         @Override
         public int getItemCount() {
-            return RECIPE_STEP.size() + 1;
+            return RECIPE_STEP.size();
         } // +1 for ingredient list
 
     }
