52673065eec7ae9005e3174033c1cd2061ec70a6
Andrew Fitzsimons <andrewsfitzsimons@gmail.com>
2017-09-05 16:24:32 +0200

    Adds LoaderManager to list activity

--- app/src/main/java/com/afitzwa/andrew/tastybakes/RecipeListActivity.java
+++ app/src/main/java/com/afitzwa/andrew/tastybakes/RecipeListActivity.java
@@ -2,7 +2,9 @@ package com.afitzwa.andrew.tastybakes;
 
 import android.appwidget.AppWidgetManager;
 import android.content.ContentResolver;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -12,16 +14,12 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import com.afitzwa.andrew.tastybakes.data.IngredientProvider;
-import com.afitzwa.andrew.tastybakes.data.RecipeColumns;
 import com.afitzwa.andrew.tastybakes.data.RecipeContent;
 import com.afitzwa.andrew.tastybakes.data.RecipeProvider;
 import com.afitzwa.andrew.tastybakes.data.StepProvider;
 import com.afitzwa.andrew.tastybakes.network.FetchUrlTask;
 import com.afitzwa.andrew.tastybakes.network.IFetchUrlTask;
 import com.afitzwa.andrew.tastybakes.widget.RecipeWidgetProvider;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -34,9 +32,12 @@ import butterknife.ButterKnife;
  * item details. On tablets, the activity presents the list of items and
  * item details side-by-side using two vertical panes.
  */
-public class RecipeListActivity extends AppCompatActivity implements IFetchUrlTask {
+public class RecipeListActivity extends AppCompatActivity
+        implements IFetchUrlTask, android.app.LoaderManager.LoaderCallbacks<Cursor> {
     private static final String TAG = RecipeListActivity.class.getSimpleName();
     private static final String RECIPE_URL = "http://go.udacity.com/android-baking-app-json";
+
+    private static final int ARG_RECIPE_LOADER_ID = 0;
 
     @BindView(R.id.recipe_list) RecyclerView mRecyclerView;
     @BindView(R.id.toolbar) Toolbar mToolbar;
@@ -48,6 +49,9 @@ public class RecipeListActivity extends AppCompatActivity implements IFetchUrlTa
 
         ButterKnife.bind(this);
 
+        mRecyclerView.setAdapter(null);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
         setSupportActionBar(mToolbar);
 
         FetchUrlTask fetchUrlTask = new FetchUrlTask(this);
@@ -58,24 +62,13 @@ public class RecipeListActivity extends AppCompatActivity implements IFetchUrlTa
         RecipeContent recipeContent = new RecipeContent();
         recipeContent.buildListFromJSONString(getContentResolver(), result);
 
+        getLoaderManager().initLoader(ARG_RECIPE_LOADER_ID, null, this);
+
         // Alert any widgets that we've updated recipes
         Intent intent = new Intent(this, RecipeWidgetProvider.class)
                 .setPackage(this.getPackageName());
         intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
         this.sendBroadcast(intent);
-
-        Cursor cursor = getContentResolver().query(RecipeProvider.Recipes.CONTENT_URI, null, null, null, null);
-        assert cursor != null;
-
-        setupRecyclerView(mRecyclerView, cursor);
-    }
-
-    public static void setupRecyclerView(@NonNull RecyclerView recyclerView, @NonNull Cursor cursor) {
-        DividerItemDecoration dividerItemDecoration =
-                new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
-        recyclerView.addItemDecoration(dividerItemDecoration);
-
-        recyclerView.setAdapter(new RecipeRecyclerViewAdapter(cursor));
     }
 
     @Override
@@ -85,5 +78,20 @@ public class RecipeListActivity extends AppCompatActivity implements IFetchUrlTa
         contentResolver.delete(IngredientProvider.Ingredients.CONTENT_URI, null, null);
         contentResolver.delete(StepProvider.Steps.CONTENT_URI, null, null);
         super.onDestroy();
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(this, RecipeProvider.Recipes.CONTENT_URI, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        mRecyclerView.setAdapter(new RecipeRecyclerViewAdapter(cursor));
+    }
+
+    @Override
+    public void onLoaderReset(android.content.Loader<Cursor> loader) {
+        mRecyclerView.setAdapter(null);
     }
 }
