e516206b0e133c9bf6c069370524a4798dd42f0e
Andrew Fitzsimons <andrewsfitzsimons@gmail.com>
2017-09-14 13:37:51 +0200

    Adds UI test and util methods

---
+++ app/src/androidTest/java/com/afitzwa/andrew/tastybakes/BasicAppTest.java
@@ -0,0 +1,94 @@
+package com.afitzwa.andrew.tastybakes;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class BasicAppTest {
+
+    @Rule
+    public ActivityTestRule<RecipeListActivity> mActivityTestRule = new ActivityTestRule<>(RecipeListActivity.class);
+
+    @Test
+    public void basicAppTest() {
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.recipe_list), isDisplayed()));
+        recyclerView.perform(actionOnItemAtPosition(0, click()));
+
+        ViewInteraction frameLayout = onView(
+                allOf(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class), isDisplayed()));
+        frameLayout.check(matches(isDisplayed()));
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withText("Recipe Introduction"), withContentDescription("step description"),
+                        childAtPosition(
+                                withId(R.id.step_list_view),
+                                0),
+                        isDisplayed()));
+        appCompatTextView.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction frameLayout2 = onView(
+                allOf(withId(R.id.player_view),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.recipe_step_container),
+                                        0),
+                                0),
+                        isDisplayed()));
+        frameLayout2.check(matches(isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
