2743d3e995a673292a71a681dd3fb6d06564236e
Andrew Fitzsimons <andrewsfitzsimons@gmail.com>
2017-09-17 09:40:03 +0200

    Restricts RLATest to check list frame appears

--- app/src/androidTest/java/com/afitzwa/andrew/tastybakes/RecipeListActivityTest.java
+++ app/src/androidTest/java/com/afitzwa/andrew/tastybakes/RecipeListActivityTest.java
@@ -1,24 +1,14 @@
 package com.afitzwa.andrew.tastybakes;
 
-import android.app.Activity;
-import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.Intents.intending;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.Matchers.allOf;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -28,33 +18,13 @@ import static org.hamcrest.Matchers.allOf;
 @RunWith(AndroidJUnit4.class)
 public class RecipeListActivityTest {
 
-    Activity mActivity;
-
     @Rule
-    public IntentsTestRule<RecipeListActivity> mActivityRule =
-            new IntentsTestRule<>(RecipeListActivity.class);
-
-
-    @Before
-    public void preTest() {
-        mActivity = mActivityRule.getActivity();
-        ContentProviderTestUtils.clearContentProvider(mActivity);
-    }
+    public ActivityTestRule<RecipeListActivity> mActivityRule =
+            new ActivityTestRule<>(RecipeListActivity.class);
 
 
     @Test
-    public void clickRecipe_startActivity() {
-
-        intending(isInternal());
-
-        onView(withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-
-        intended(allOf(hasComponent(RecipeDetailActivity.class.getName())));
-    }
-
-
-    @After
-    public void postTest() {
-        ContentProviderTestUtils.clearContentProvider(mActivity);
+    public void recipeListExists() {
+        isDisplayed().matches(withId(R.id.recipe_list));
     }
 }
