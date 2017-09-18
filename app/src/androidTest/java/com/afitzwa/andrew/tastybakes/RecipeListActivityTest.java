8088d1524f13f13bbd3621c817ae7d67c10b2313
Andrew Fitzsimons <andrewsfitzsimons@gmail.com>
2017-09-18 09:26:24 +0200

    Changes to IntentTestRule, checks intent on click

--- app/src/androidTest/java/com/afitzwa/andrew/tastybakes/RecipeListActivityTest.java
+++ app/src/androidTest/java/com/afitzwa/andrew/tastybakes/RecipeListActivityTest.java
@@ -1,12 +1,17 @@
 package com.afitzwa.andrew.tastybakes;
 
-import android.support.test.rule.ActivityTestRule;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
@@ -19,12 +24,18 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 public class RecipeListActivityTest {
 
     @Rule
-    public ActivityTestRule<RecipeListActivity> mActivityRule =
-            new ActivityTestRule<>(RecipeListActivity.class);
-
+    public IntentsTestRule<RecipeListActivity> mRule =
+            new IntentsTestRule<>(RecipeListActivity.class);
 
     @Test
     public void recipeListExists() {
         isDisplayed().matches(withId(R.id.recipe_list));
+    }
+
+    @Test
+    public void clickRecipe_StartDetailActivity() {
+        onView(withId(R.id.recipe_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        intended(toPackage("com.afitzwa.andrew.tastybakes"));
     }
 }
